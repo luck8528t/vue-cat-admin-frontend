65f224c7fdcd95f459389a335958a3355baf81b4
fdf@qibo.com <fdf@qibo.com>
2026-06-03 10:47:09 +0800

    edit

--- assets/ad-type-187147dd.js
+++
@@ -1,1 +0,0 @@
-import{h as t}from"./request-518ab260.js";function p(){return t.get("/ad/type/list")}export{p as g};
---
+++ assets/company-2a711bf6.js
@@ -0,0 +1,1 @@
+import{h as t}from"./request-48b3fde4.js";function n(){return t.get("/company/list",{_t:Date.now()})}export{n as g};
